75c4607b24e9e09d3955da0855570c7dca2b6a08
Mankong <mankong@mankong.me>
2025-02-18 21:02:21 +0800

    test 1

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 const App = () => {
   return (
     <div className="content">
-      <h1>Rsbuild with Preact</h1>
+      <h1>Rsbuild with Preact flow-h5-1</h1>
       <p>Start building amazing things with Rsbuild.</p>
     </div>
   );
